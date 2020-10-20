f04ec2a255901ce38547d5ad98fdf1e5616ca3d6
Friedrich von Never <friedrich@fornever.me>
2020-10-20 23:06:21 +0700

    Fix #5: filter old chat events

---
+++ CaptchaBot.Tests/WelcomeServiceTests.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CaptchaBot.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CaptchaBot.Tests
+{
+    public class WelcomeServiceTests
+    {
+        private readonly UsersStore _usersStore = new UsersStore();
+        private readonly Mock<ITelegramBotClient> _botMock = new Mock<ITelegramBotClient>();
+        private readonly ILogger<WelcomeService> _logger;
+
+        public WelcomeServiceTests(ITestOutputHelper outputHelper)
+        {
+            _logger = outputHelper.BuildLoggerFor<WelcomeService>();
+            _botMock.Setup(b => b.SendTextMessageAsync(
+                It.IsAny<ChatId>(),
+                It.IsAny<string>(),
+                It.IsAny<ParseMode>(),
+                It.IsAny<bool>(),
+                It.IsAny<bool>(),
+                It.IsAny<int>(),
+                It.IsAny<IReplyMarkup>(),
+                It.IsAny<CancellationToken>()
+            )).ReturnsAsync(new Message());
+        }
+
+        private static Task ProcessNewChatMember(WelcomeService service, int userId, DateTime enterTime)
+        {
+            var testUser = new User {Id = userId}; 
+            var message = new Message
+            {
+                Date = enterTime,
+                Chat = new Chat(),
+                From = new User(),
+                NewChatMembers = new[]
+                {
+                    testUser
+                }
+            };
+            return service.ProcessNewChatMember(message);
+        }
+
+        [Fact]
+        public async Task BotShouldProcessEventWithinTimeout()
+        {
+            var config = new AppSettings {ProcessEventTimeout = TimeSpan.FromSeconds(5.0)};
+            var welcomeService = new WelcomeService(config, _usersStore, _logger, _botMock.Object);
+
+            const int testUserId = 123;
+            await ProcessNewChatMember(welcomeService, testUserId, DateTime.UtcNow);
+
+            Assert.Collection(_botMock.Invocations, 
+                restrict => Assert.Equal(nameof(ITelegramBotClient.RestrictChatMemberAsync), restrict.Method.Name),
+                sendMessage => Assert.Equal(nameof(ITelegramBotClient.SendTextMessageAsync), sendMessage.Method.Name));
+            
+            Assert.Equal(testUserId, _usersStore.GetAll().Single().Id);
+        }
+        
+        [Fact]
+        public async Task BotShouldNotProcessEventOutsideTimeout()
+        {
+            var config = new AppSettings {ProcessEventTimeout = TimeSpan.FromMinutes(5.0)};
+            var welcomeService = new WelcomeService(config, _usersStore, _logger, _botMock.Object);
+            
+            const int testUserId = 345;
+            await ProcessNewChatMember(welcomeService, testUserId, DateTime.UtcNow - TimeSpan.FromMinutes(6.0));
+            
+            Assert.Empty(_botMock.Invocations);
+            Assert.Empty(_usersStore.GetAll());
+        }
+    }
+}
